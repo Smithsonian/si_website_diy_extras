0949daa1d64767b322c60b295073a20dd024597d
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-10-22 16:38:43 -0400

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,8 @@ configure({ tagId: 'G-CQCJ1860S7' });
 
 <template>
   <BApp>
-    <h1>You did it!</h1>
-    <p>
-      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-      documentation
-    </p>
+    <header>
+      <DiyExtrasNavBar />
+    </header>
   </BApp>
 </template>
---
+++ src/components/DiyExtrasNavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <BNavbar v-b-color-mode="'dark'" variant="dark" container="xl">
+    <BNavbarBrand href="https://datalabs.cfa.harvard.edu/">
+      <img src="/src/assets/DataLabsLogo@2x.webp" height="45" width="195" />
+    </BNavbarBrand>
+    <BNavbarNav>
+      <BNavText>
+        <span class="vr bg-light text-light ms-5 me-3 h-100 align-top"></span>
+        <img
+          class="d-inline-block align-top"
+          src="/src/assets/diy_extras_logo.svg"
+          height="36"
+          width="415"
+        />
+      </BNavText>
+    </BNavbarNav>
+    <BNavbarNav class="ms-auto">
+      <BNavItem :href="DIY_URL" variant="light" opacity-hover="75" class="text-uppercase">
+        DIY Planet Search
+      </BNavItem>
+    </BNavbarNav>
+  </BNavbar>
+</template>
+
+<script setup lang="ts">
+import { DIY_URL } from '@/constants';
+</script>
