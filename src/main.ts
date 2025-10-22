caabd9f26d54b1a31c8799320b2c261327cd5a13
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-10-22 14:00:02 -0400

    Bootstrap-ify, partly

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import { ViteSSG } from 'vite-ssg/single-page';
 
-const app = createApp(App)
-
-app.use(createPinia())
-
-app.mount('#app')
+export const createApp = ViteSSG(App, ({ app }) => {
+  app.use(createPinia());
+});
